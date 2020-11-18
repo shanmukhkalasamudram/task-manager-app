a984eb188d750c58f6ca1a409ffab2e3b18e8e03
shanmukhkalasamudram <shanmukhkalasamudram@gmail.com>
2020-11-18 23:25:34 +0530

    jest testing added for users

--- src/index.js
+++ src/index.js
@@ -1,70 +1,6 @@
-const express = require('express')
-require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
-const { update } = require('./models/user')
-//const multer = require('multer')
+const app = require('./app')
 
-const userRouter = require('./routers/user')
-
-const taskRouter = require('./routers/task')
-
-const app = express()
 const port = process.env.PORT 
-
-const multer = require('multer')
-
-
-const upload = multer({
-    dest: 'image'
-})
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-})
-
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
-
-
-
-app.use(express.json())
-
-app.use(userRouter)
-
-app.use(taskRouter)
-
-
-// const router = new express.Router()
-
-// router.get('/test', (req,res) => {
-//     res.send('thos is from my router ')
-// })
-
-// app.use(router)
-
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async() => {
-    const token = jwt.sign({ _id: 'abc123'}, 'thiis', {expiresIn: '7 days'})
-
-    console.log(token) 
-
-    const data = jwt.verify(token,'thiis')
-
-    console.log(data)
-
-
-    
-}
-
-
-myFunction()
-
-
-
 
 
 app.listen(port, () => {
@@ -73,17 +9,88 @@ app.listen(port, () => {
 
 
 
+// const express = require('express')
+// require('./db/mongoose')
+// const User = require('./models/user')
+// const Task = require('./models/task')
+// const { update } = require('./models/user')
+// //const multer = require('multer')
 
-// //const Task = require('./models/task')
-// //const User = require('./models/user')
+// const userRouter = require('./routers/user')
+
+// const taskRouter = require('./routers/task')
+
+// const app = express()
 
 
-// const main = async () => {
-//     const user = await User.findById('5fb242bb13f02805704d8ca3')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
+// const multer = require('multer')
+
+
+// const upload = multer({
+//     dest: 'image'
+// })
+// app.post('/upload', upload.single('upload'), (req, res) => {
+//     res.send()
+// })
+
+
+// // app.use((req, res, next) => {
+// //     res.status(503).send('Site is currently down. Check back soon!')
+// // })
+
+
+
+// app.use(express.json())
+
+// app.use(userRouter)
+
+// app.use(taskRouter)
+
+
+// // const router = new express.Router()
+
+// // router.get('/test', (req,res) => {
+// //     res.send('thos is from my router ')
+// // })
+
+// // app.use(router)
+
+
+// const jwt = require('jsonwebtoken')
+
+// const myFunction = async() => {
+//     const token = jwt.sign({ _id: 'abc123'}, 'thiis', {expiresIn: '7 days'})
+
+//     console.log(token) 
+
+//     const data = jwt.verify(token,'thiis')
+
+//     console.log(data)
+
+
     
 // }
 
-// main()
+
+// myFunction()
+
+
+
+
+
+
+
+
+// // //const Task = require('./models/task')
+// // //const User = require('./models/user')
+
+
+// // const main = async () => {
+// //     const user = await User.findById('5fb242bb13f02805704d8ca3')
+// //     await user.populate('tasks').execPopulate()
+// //     console.log(user.tasks)
+    
+// // }
+
+// // main()
 
